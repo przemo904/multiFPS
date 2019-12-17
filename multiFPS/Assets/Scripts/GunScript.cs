99880d4765fe442ee848fe3795c24e34dd8e03ac
przemo904 <przemosoft904@gmail.com>
2019-12-17 22:54:33 +0100

    Add Shooting mechanics
    Shooting using raycast

---
+++ multiFPS/Assets/Scripts/GunScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public float gunDamage = 100f;
+    public Camera fpsCam;
+    public float weaponRange = 50f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    private void Shoot() {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, weaponRange))
+        {
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null) {
+                target.TakeDamage(gunDamage);
+            }
+
+        }
+
+    }
+
+
+}
