f58ac7d7fbf0b65a2965ebe0f9da904e77430f87
przemo904 <przemosoft904@gmail.com>
2019-12-19 12:49:19 +0100

    Add Visual Damage

--- multiFPS/Assets/Scripts/Target.cs
+++ multiFPS/Assets/Scripts/Target.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections;
 
 public class Target : MonoBehaviour
 {
 
     public float health = 100f;
 
+    private WaitForSeconds visualTime = new WaitForSeconds(0.1f);
+    private new Renderer renderer;
+    private Color baseColor;
+    private void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+        baseColor = renderer.material.GetColor("_Color");
+    }
+
     public void TakeDamage(float amount) {
         health -= amount;
+
+        StartCoroutine(visualDamage());
 
         if(health <= 0)
         {
@@ -17,6 +30,15 @@ public class Target : MonoBehaviour
     void Die() {
 
         Destroy(gameObject);
+    }
+
+    private IEnumerator visualDamage() {
+
+        Debug.Log("trafiony");
+        renderer.material.SetColor("_Color", Color.red);
+        yield return visualTime;
+        renderer.material.SetColor("_Color", baseColor);
+        Debug.Log("zatopiony");
     }
 
 }
